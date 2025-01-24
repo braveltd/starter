56b2ec108b163a16842efd3ed804def39bb08ccf
braveltd <belicoff0505@gmail.com>
2025-01-24 15:50:19 +0300

    feat: added typr siduck's plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -356,6 +356,13 @@ return {
     },
   },
 
+  {
+    "nvzone/typr",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = { "Typr", "TyprStats" },
+  },
+
   "NvChad/nvcommunity",
 
   { import = "nvcommunity.editor.telescope-undo" },
