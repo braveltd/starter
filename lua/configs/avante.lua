079dc815100a4890b3b946ba8c11426f2fec732c
braveltd <belicoff0505@gmail.com>
2025-01-28 00:28:25 +0300

    feat: added typr

--- lua/configs/avante.lua
+++ lua/configs/avante.lua
@@ -1,22 +1,10 @@
-local function read_system_prompt()
-  local path = vim.fn.expand "~/.config/nvim/prompts/avante_system.txt"
-  local file = io.open(path, "r") -- Open the file in read mode
-  if not file then
-    return ""
-  end
-  local content = file:read "*a" -- Read all content
-  file:close()
-  return content
-end
-
 require("avante").setup {
-  system_prompt = read_system_prompt(),
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
   provider = "openai",
   openai = {
     endpoint = "https://api.hyperbolic.xyz/v1",
     model = "deepseek-ai/DeepSeek-R1",
-    max_tokens = 131072,
+    max_tokens = 96777,
     temperature = 0.1,
   },
   behaviour = {
